79f3a908f6899491f62effdbaf02b291ed1ae654
aldi7id <aldi7.indo@gmail.com>
2022-04-14 00:00:19 +0700

    add @POST

--- app/src/main/java/com/ajgroup/json/service/ApiService.kt
+++ app/src/main/java/com/ajgroup/json/service/ApiService.kt
@@ -1,10 +1,18 @@
 package com.ajgroup.json.service
 
 import com.ajgroup.json.model.GetAllCarResponseItem
+import com.ajgroup.json.model.PostRegisterResponse
+import com.ajgroup.json.model.RegisterRequest
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiService {
     @GET("admin/car")
     fun getAllCar(): Call<List<GetAllCarResponseItem>>
+
+
+    @POST("admin/auth/register")
+    fun postRegister(@Body request: RegisterRequest): Call <PostRegisterResponse>
 }
